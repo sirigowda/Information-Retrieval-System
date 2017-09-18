048b42a06011202b66b0f7c9198eead780670c01
Siri Gowda <sirigowd@usc.edu>
2017-09-18 03:57:28 -0700

    Update PageRank.py

--- PageRank.py
+++ PageRank.py
@@ -1,10 +1,11 @@
 
 import networkx as nx;
 
-G = nx.read_edgelist("/home/siri/Downloads/solr-6.5.0/edgeList.txt", create_using=nx.DiGraph())
+baseFilePath="/home/siri/Downloads/solr-6.5.0/"
+G = nx.read_edgelist(baseFilePath + "edgeList.txt", create_using=nx.DiGraph())
 pr = nx.pagerank(G, alpha=0.85, personalization=None, max_iter=100, tol=1e-06, nstart=None, weight='weight', dangling=None)
 
-with open("/home/siri/Downloads/solr-6.5.0/PageRankExternal2.txt", 'w') as f:
+with open(baseFilePath + "PageRankExternal2.txt", 'w') as f:
     for key, value in pr.items():
-        key = "/home/siri/Downloads/solr-6.5.0/server/solr/ABCNewsData" + key
+        key = baseFilePath + "server/solr/ABCNewsData" + key
         f.write('%s=%s\n' % (key, value))
